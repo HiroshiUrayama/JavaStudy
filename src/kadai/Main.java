3df72ae8e49ad8011ab6021bc2bd2a159e2d51c1
HiroshiUrayama <hiroshiurayama0308@gmail.com>
2020-04-18 09:08:30 +0900

    Add greetingHelloWorld_two_first.java & renew Main
    課題2-1作成完了したため

--- src/kadai/Main.java
+++ src/kadai/Main.java
@@ -1,7 +1,13 @@
 package kadai;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("こんにちは");
+		System.out.println("課題番号を入力してください。");
+		Scanner scan = new Scanner(System.in);
+		if(scan.nextInt() == 1) {
+			greetingHelloWorld_two_first.greeting();
+		}
 	}
 }
---
+++ src/kadai/greetingHelloWorld_two_first.java
@@ -0,0 +1,9 @@
+package kadai;
+
+public class greetingHelloWorld_two_first{
+
+	public static void greeting() {
+		String greeting = "HelloWorld";
+		System.out.println(greeting);
+	}
+}
