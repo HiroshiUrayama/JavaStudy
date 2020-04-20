2097800687ab5233cbdc7740bc17864babdc1a91
Hiroshi Urayama <hiroshiurayama0308@gmail.com>
2020-04-21 01:40:22 +0900

    ReNew Kadai1～5 , DisplayOfMap , scan.close() Delete
    全体を更新、再帰呼び出しでエラーになったscan.close()をコメントアウト

---
+++ src/kadai/Communication_two_sixth.java
@@ -0,0 +1,5 @@
+package kadai;
+
+public interface Communication_two_sixth {
+	//interface作成のみ
+}
--- src/kadai/Main.java
+++ src/kadai/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		practiceCheck();
 	}
-	
+
 	//課題選択メソッド(1~12が課題番号に対応、13は前処理、14は停止、再帰呼び出しする)
 	static void practiceCheck() {
 		System.out.println("課題番号を入力してください。");
@@ -13,9 +13,6 @@ public class Main {
 
 		//受け皿を用意する
 		int practiceNo = scan.nextInt();
-
-		//リソース・リーク防止(Stackflow懸念を消しておきたいんだが...NosuchElementExceptionが出る)
-		//scan.close();
 
 		//入力した数値に応じて課題クラスに分岐する
 		if(practiceNo == 1) {
@@ -27,21 +24,21 @@ public class Main {
 		} else if(practiceNo == 4) {
 			Judge_two_fourth.randomNumber();
 		} else if(practiceNo == 5) {
-			DisplayOfMap.askDisplayNumber();
+			DisplayOfMap_two_fifth.askDisplayNumber();
 		} else if(practiceNo == 13) {
 			//一気に実行
 			greetingHelloWorld_two_first.greeting();
 			Increment_two_second.fourFor();
-			Threeyearslatertoday_two_third.threeYearslatertoday();			
+			Threeyearslatertoday_two_third.threeYearslatertoday();
 			Judge_two_fourth.randomNumber();
-			DisplayOfMap.askDisplayNumber();
+			DisplayOfMap_two_fifth.askDisplayNumber();
 		} else if(practiceNo == 14) {
 			//終了させる
 			System.out.println("処理を終了します、見ていただいてありがとうございました！");
 			scan.close();
 			return;
 		} else {
-			System.out.println("1~14までの数字を入れてね！");			
+			System.out.println("1~14までの数字を入れてね！");
 		}
 	practiceCheck();
 	//閉じる時に開放しておく
